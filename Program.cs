566b70eb5b7fc59357ce9d4f2a65cb5e5421cfe2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 20:28:45 -0300

    Finaliza exercício

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Contando até dez:");
+Console.Beep();
+Console.Write(" 0");
+Thread.Sleep(1000);
+Console.Beep();
+Console.Write(" 1");
+Thread.Sleep(1000);
+Console.Beep();
+Console.Write(" 2");
+Thread.Sleep(1000);
+Console.Beep();
+Console.Write(" 3");
+Thread.Sleep(1000);
+Console.Beep();
+Console.Write(" 4");
+Thread.Sleep(1000);
+
+Console.Beep();
+Console.Write(" 5");
+Thread.Sleep(1000);
+
+Console.Beep();
+Console.Write(" 6");
+Thread.Sleep(1000);
+
+Console.Beep();
+Console.Write(" 7");
+Thread.Sleep(1000);
+
+Console.Beep();
+Console.Write(" 8");
+Thread.Sleep(1000);
+
+Console.Beep();
+Console.Write(" 9");
+Thread.Sleep(1000);
+
+Console.Beep();
+Console.Write(" 10");
+Thread.Sleep(1000);
